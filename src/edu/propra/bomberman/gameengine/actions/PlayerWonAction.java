4c65cb5867503ea521ac17faafc1719d793ac1f6
Nadescha Rudat <Nadescha.Rudat@gmx.de>
2012-06-22 11:17:01 +0200

    added some scenes

--- src/edu/propra/bomberman/gameengine/actions/PlayerWonAction.java
+++ src/edu/propra/bomberman/gameengine/actions/PlayerWonAction.java
@@ -1,5 +1,6 @@
 package edu.propra.bomberman.gameengine.actions;
 
+import edu.propra.bomberman.gameengine.SGameEngine;
 import edu.propra.bomberman.gameengine.objects.Player;
 
 public class PlayerWonAction extends ActionObject {
@@ -10,7 +11,8 @@ public class PlayerWonAction extends ActionObject {
 	}
 	@Override
 	public void action() {
-		System.out.println(((Player)actor).getName()+" has won the game");
+		System.out.println(((Player)actor).getName()+" has won the game");;
+		SGameEngine.get().endGame();
 
 	}
 
