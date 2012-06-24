4f874e6f36e699afcdfe1c63b8ebf11bb3d16950
alhep100 <alex.hepting@uni-duesseldorf.de>
2012-06-24 18:58:33 +0200

    AudioJukebox

---
+++ src/edu/propra/bomberman/audio/Jukebox.java
@@ -0,0 +1,40 @@
+package edu.propra.bomberman.audio;
+
+import java.net.URL;
+import java.applet.*;
+
+public class Jukebox {
+	private AudioClip[] sounds; // AudioClips
+
+	public Jukebox() {
+		initSounds();
+	}
+
+	private void initSounds() {
+		try {
+			// needed for correct loading of resources in JAR-Files
+			ClassLoader loader = Jukebox.class.getClassLoader();
+			// load AudioClips
+			sounds = new AudioClip[2];
+			sounds[0] = Applet.newAudioClip(loader.getResource("src/resources/gun.wav"));
+			sounds[1] = Applet.newAudioClip(loader.getResource("src/resources/roll.wav"));
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	public void playSound(int index) {
+		if (index > 0 && index < sounds.length)
+			sounds[index].play();
+	}
+
+	public static void main(String[] args) {
+		Jukebox jukebox = new Jukebox(); // initialize jukebox
+		jukebox.playSound(0); // play first sound
+		try {
+			Thread.sleep(100); // wait a bit
+		} catch (InterruptedException ex) {
+		}
+		jukebox.playSound(1); // play second sound
+	}
+}
