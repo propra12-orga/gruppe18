015c2a3e604114c40bd38e49302d0140ff5ebf38
jens herrmann <jens.herrmann@uni-duesseldorf.de>
2012-06-29 23:28:01 +0200

    überarbeitete networkengine

---
+++ src/edu/propra/bomberman/networkengine/BombermanServer.java
@@ -0,0 +1,10 @@
+package edu.propra.bomberman.networkengine.netzwerk;
+
+public class BombermanServer extends NetzwerkClassAlt {
+
+	public static void main(String[] args) {
+		Server = true;
+		NetzwerkClassAlt.ServerSocket();
+	}
+
+}
